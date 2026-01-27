270d388e07af4bac85e719895c20b264d69e458b
Majaahh <maja.blaszczak@proton.me>
2026-01-27 17:35:04 +0100

    scripts: Initial flash script
    Change-Id: Ib1981f6f85fb0487645fafb8c9ca40e237650326
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ scripts/flash.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: Majaahh
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# [
+source "$SRC_DIR/scripts/utils/common_utils.sh"
+source "$SRC_DIR/scripts/utils/log_utils.sh"
+
+_PRINT_USAGE()
+{
+    echo "Usage: flash.sh <part> <file>"
+    echo "Parts:"
+    echo "-a (AP)"
+    echo "-b (BL)"
+    echo "-s (CSC)"
+    echo "-c (CP)"
+}
+
+PART="$1"
+FILE="$2"
+# ]
+
+if [[ "$#" -lt "2" ]]; then
+    _PRINT_USAGE
+    exit 1
+fi
+
+if ! type odin4 &>/dev/null; then
+    LOGE "Odin4 was not found. Please download and add it to your path!"
+    exit 1
+fi
+
+if [[ "$PART" != "-a" ]] && [[ "$PART" != "-b" ]] && \
+    [[ "$PART" != "-s" ]] && [[ "$PART" != "-c" ]]; then
+    LOGE "$PART is not a valid part"
+    _PRINT_USAGE
+    exit 1
+fi
+
+if [[ ! -f "$FILE" ]]; then
+    LOGE "$FILE was not found"
+    exit 1
+fi
+
+if ! lsusb | grep -q "GT-I9100"; then
+    LOGE "Device was not found. Is it in download mode?"
+    exit 1
+fi
+
+EVAL "odin4 \"$PART\" \"$FILE\""
