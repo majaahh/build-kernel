723768f2466608a0a4b53b61d8adf38d88f2e0d2
Majaahh <maja.blaszczak@proton.me>
2026-01-28 19:22:34 +0100

    scripts: Null adb output
    Change-Id: Ie832d85bd803d9ee416e7151f26db9f02feae464
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -47,7 +47,7 @@ if [[ ! -f "$FILE" ]]; then
     exit 1
 fi
 
-if adb devices | grep -Ewq "device|recovery"; then
+if adb devices | grep -Ew "device|recovery" &>/dev/null; then
     LOG "- Rebooting device to download mode"
     EVAL "adb reboot download"
 fi
