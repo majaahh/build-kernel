43bd0953ba563cf5d5f35ec1eb5dd3f1ae94e71b
Majaahh <maja.blaszczak@proton.me>
2026-04-19 18:19:19 +0200

    scripts: build_kernel: Inform when latest AOSP Clang version could not be fetched
    Change-Id: I409c0b00a52218453aece6d1bc0448e25edfca27
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- scripts/build_kernel.sh
+++ scripts/build_kernel.sh
@@ -81,8 +81,10 @@ fi
 if [[ ! -d "$TOOLCHAIN_DIR" ]]; then
     GET_AOSP_CLANG || exit 1
 else
-    if [[ "$(GET_LATEST_AOSP_CLANG | sed "s/clang-//g")" != "$(awk -F'"' '/"tag"/ {print $4}' "$TOOLCHAIN_DIR/BUILD_INFO")" ]]; then
+    if [[ -n "$(GET_LATEST_AOSP_CLANG)" ]] && [[ "$(GET_LATEST_AOSP_CLANG | sed "s/clang-//g")" != "$(awk -F'"' '/"tag"/ {print $4}' "$TOOLCHAIN_DIR/BUILD_INFO")" ]]; then
         LOG "\033[0;33m! Newer AOSP Clang is available ($(awk -F'"' '/"tag"/ {print $4}' "$TOOLCHAIN_DIR/BUILD_INFO") -> ${LATEST_AOSP_CLANG//clang-/})\033[0m"
+    else
+        LOG "\033[0;33m! Failed to fetch latest AOSP Clang version\033[0m"
     fi
 fi
 
